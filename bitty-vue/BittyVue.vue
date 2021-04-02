21fe930a5a6f577a45d44d34caf20d363d56f691
jingdor <945627077@qq.com>
2021-04-02 17:30:11 +0800

    '更新'

--- bitty-vue/BittyVue.vue
+++ bitty-vue/BittyVue.vue
@@ -3,3 +3,17 @@
         hello bitty Vue
     </div>
 </template>
+
+<script>
+export default {
+    name:"bitty",
+    data(){
+        return {
+            broker:null
+        }
+    },
+    mounted: function () {
+        console.log("mounted");
+    }
+}
+</script>
